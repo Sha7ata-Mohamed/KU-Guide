de37904d03abdee8a979ad1933e842a33bdfb455
Sha7ata-Mohamed <sha7ata1379@gmail.com>
2025-11-05 19:37:03 +0300

    Done
    Done with the setting up the structure

--- Kuwait_University/KUGuide/urls.py
+++ Kuwait_University/KUGuide/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('faq/', views.faq, name='faq'),
     path('career/', views.career, name='career'),
+    path('profile/', views.profile, name='profile'),
     path('contact/', views.contact, name='contact'),
+    path('signin/', views.signin, name='signin'),
 ]
